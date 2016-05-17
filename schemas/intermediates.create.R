49b3d2f143fef1ad1b308a2bf209e8c16957af20
Jonathan Claus <jonathanmclaus@gmail.com>
2016-05-17 15:45:18 -0400

    Initial commit

---
+++ schemas/intermediates.create.R
@@ -0,0 +1,7 @@
+library(gtBase)
+
+Create(groupjoin_intermediates,
+       Predicate = base::Factor(dict = "fb_predicate", bytes = 4),
+       Subject   = base::Int,
+       Object    = base::Int,
+       Rule      = base::Int)
