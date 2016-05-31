ceecd3341509c7646ea81f745438b9077624ac3a
Jonathan Claus <jonathanmclaus@gmail.com>
2016-05-31 16:54:49 -0400

    Added template arguments

--- package/R/GroupJoin.R
+++ package/R/GroupJoin.R
@@ -1,8 +1,8 @@
-GroupJoin <- function(states, outputs) {
+GroupJoin <- function(states, outputs, fragment.size = 2E6) {
   outputs <- substitute(outputs)
   check.atts(outputs)
   outputs <- convert.atts(outputs)
 
-  gist <- GIST(learning::GroupJoin)
+  gist <- GIST(learning::GroupJoin, fragment.size)
   Transition(gist, outputs, states)
 }
