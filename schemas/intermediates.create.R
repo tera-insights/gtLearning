7e9f243d26dc1407134ee65c6a27788f069da8d5
Jonathan Claus <jonathanmclaus@gmail.com>
2016-05-31 16:57:33 -0400

    Added distinct intermediates

--- schemas/intermediates.create.R
+++ schemas/intermediates.create.R
@@ -5,3 +5,9 @@ Create(groupjoin_intermediates,
        Subject   = base::Int,
        Object    = base::Int,
        Rule      = base::Int)
+
+Create(distinct_intermediates,
+       Predicate = base::Factor(dict = "fb_predicate", bytes = 4),
+       Subject   = base::Int,
+       Object    = base::Int,
+       Rule      = base::Int)
