6973813b470253bee87c44cfaa9c4163d80e57ed
Jonathan Claus <jonathanmclaus@gmail.com>
2016-05-31 16:54:32 -0400

    Added template arguments, updated error messages

--- package/R/Group.R
+++ package/R/Group.R
@@ -1,27 +1,30 @@
-Group <- function(data, keys, values) {
+Group <- function(data, keys, values, use.array = FALSE, fragment.size = 2E6,
+                  debug = 0, delete.contents = FALSE) {
   keys <- convert.exprs(substitute(keys))
   vals <- convert.exprs(substitute(values))
 
   split <- length(keys)
-  gla <- GLA(learning::Group, split)
+  if (!is.logical(use.array) && length(use.array) == 1)
+    stop("Group: use.array should be a single boolean.")
+  gla <- GLA(learning::Group, split, use.array, fragment.size, debug, delete.contents)
 
   key.names <- convert.names(keys)
   missing <- which(key.names == "")
   exprs <- grokit$expressions[keys[missing]]
-  if (all(is.symbols(exprs)))
+  if (all(good <- is.symbols(exprs)))
     key.names[missing] <- as.character(exprs)
   else
     stop("no name given for complex inputs:",
-         paste("\n\t", lapply(keys[missing], deparse), collapse = ""))
+         paste("\n\t", lapply(exprs[!good], deparse), collapse = ""))
 
   val.names <- convert.names(vals)
   missing <- which(val.names == "")
   exprs <- grokit$expressions[vals[missing]]
-  if (all(is.symbols(exprs)))
+  if (all(good <- is.symbols(exprs)))
     val.names[missing] <- as.character(exprs)
   else
     stop("no name given for complex inputs:",
-         paste("\n\t", lapply(vals[missing], deparse), collapse = ""))
+         paste("\n\t", lapply(exprs[!good], deparse), collapse = ""))
 
   Aggregate(data, gla, c(keys, vals), c(key.names, val.names))
 }
