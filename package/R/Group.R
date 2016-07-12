f6c7c51cea7e3e026f7aced195e7f28bd9e1903e
Jonathan Claus <jonathanmclaus@gmail.com>
2016-07-12 17:31:54 -0400

    Added R documentation

--- package/R/Group.R
+++ package/R/Group.R
@@ -1,6 +1,46 @@
-## If no actual keys are given, a dummy key is 1 is used instead. The algorithm
-## could be specifically optimizied for the absence of keys, but the benefit is
-## most likely not worth the effort.
+#' Grouping GLA
+#'
+#' \code{Group} is used to group data based on a natural ordering of its keys
+#' and then output it in that order.
+#'
+#' This GLA takes in two types of expressions - keys and values. For each
+#' distinct combination of keys, it stores all the values associated with those
+#' keys in the order that they are seen. It then splits the keyset into chunks
+#' and outputs their values separately. Furthermore, the output for the keys in
+#' the same chunk is ordered by the natural ordering of the keys, with the first
+#' key given the most precedence and so on. The ordering of the values for a
+#' single set of keys is usually arbitrary due to the execution of the GLA and
+#' therefore it should not be relied upon.
+#'
+#' The GLA can update the stored values for the same set of keys simultaneously,
+#' meaning that the runtime is not significantly impacted by a keyset with few
+#' unique values.
+#'
+#' @param data A \code{\link{waypoint}}.
+#' @param keys A named list of expressions, whose names are used for the output
+#'   column associated with that key. If no name is given and the corresponding
+#'   key is simply an attribute of \code{data}, then the name of that attribute
+#'   is also used for the output. Any key more complex than a single attribute
+#'   must be given a name.
+#'
+#'   If no keys are given, a dummy expression always equal to 1 is used instead.
+#'   In this case, the GLA simply accumulates all the data in a mostly arbitrary
+#'   order.
+#' @param values A named list of expressions that specify the values to be
+#'   stored alongside the keys. The specification format follows the same rules
+#'   as \code{keys}.
+#' @param fragment.size The number of distinct combination of keys used in each
+#'   chunk of output.
+#' @param debug An integer code used to specify the level of debugging messages.
+#' @param key.array A boolean specifying whether the keys are stored as an array
+#'   or a tuple. This is only important when using this GLA as an input state.
+#' @param val.array The same as \code{key.array} but for the storage of values.
+#' @param delete.contents Whether the PostFinalize method is used to delete the
+#'   stored information. This is only relevant when using this GLA as a state.
+#'   If false, the stored data is kept for the entire query, which can be a
+#'   wasteful use of memory.
+#' @return A \code{\link{waypoint}} with the designated columns and rows.
+#' @author Jon Claus, <jonterainsights@@gmail.com>, Tera Insights, LLC.
 Group <- function(data, keys = c(group = 1), values, fragment.size = 2E6, debug = 0,
                   key.array = FALSE, val.array = FALSE, delete.contents = FALSE) {
   keys <- convert.exprs(substitute(keys))
--- package/R/GroupJoin.R
+++ package/R/GroupJoin.R
@@ -1,3 +1,39 @@
+#' GroupJoin GIST
+#'
+#' \code{GroupJoin} is used to perform the GroupJoin operation, using a GIST
+#' waypoint.
+#'
+#' This GIST performs the GroupJoin operation using two states. The first state
+#' should be a Multiplexer placed on top of one or two Segmenter GLAs, each
+#' with the inner GLA being Group. Each of these Groups should represent a
+#' mapping of subject -> predicate -> object (SPO) or vice versa (OPS). In the
+#' case that both types of mapping are present, SPO should come first. In each
+#' case, the expression which the Segmenter acts upon should be the one used for
+#' the grouping, e.g. subject for SPO and object for OPS. These states represent
+#' the knowledge base, which preprocessing done in the form of grouping.
+#'
+#' The second and final state is also a Segmenter on top of a Group GLA. This
+#' should represent a mapping of (r.ID, r.head, r.body1, r.body2) -> arguments,
+#' where each argument for a rule is a value z such that that rule infers at
+#' least one new fact when z is the shared variable of the two body predicates.
+#'
+#' For example, the type 4 rule (ID, p, q, r) should be mapped to (a, b) if the
+#' knowledge base contains: \code{q(A, a) r(a, B) q(C, b) r(b, D)}. This is
+#' because the rule can infer \code{r(A, B)} and \code{r(C, D)}, when the common
+#' variable of \code{q} and \code{r} is \code{a} and \code{b}, respectively.
+#'
+#' Please see the examples for a demonstration of usage.
+#'
+#' @param states A list of two \code{\link{waypoint}}s, the facts and rules
+#'   states. See \code{Details} for more information.
+#' @param outputs An attribute list specifying the names of the columns for the
+#'   subject, predicate, object, and rule ID of the output, in that order.
+#' @param rule.type Which type of rule is being used for inference.
+#' @param fragment.size The output is separated into chunks, with this many
+#'   rules per chunk.
+#' @return A \code{\link{waypoint}} with the designated columns and rows.
+#' @author Jon Claus, <jonterainsights@@gmail.com>, Tera Insights, LLC.
+#' @references \url{http://www.cise.ufl.edu/~yang/doc/sigmod16.pdf}
 GroupJoin <- function(states, outputs, rule.type, fragment.size = 2E6) {
   outputs <- substitute(outputs)
   check.atts(outputs)
